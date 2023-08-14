7edf9227ab57c76c878c9d319a135341d84f12c7
高浩 <gaohao.wherever@bytedance.com>
2023-08-14 14:25:08 +0800

    add flash

--- lua/plugin/init.lua
+++ lua/plugin/init.lua
@@ -129,6 +129,20 @@ require('lazy').setup({
 
     'nvim-lua/popup.nvim',
     'nvim-telescope/telescope-media-files.nvim',
+
+    {
+      "folke/flash.nvim",
+      event = "VeryLazy",
+      opts = {},
+      version = '1.17.3',
+      keys = {
+        { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+        { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+        { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+        { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+        { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+      },
+    }
 }
 )
 
